3579003e51635684501fe1b4b318e94850ff19a9
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-03-01 16:37:54 -0600

    add all file to the project

---
+++ Roshambo/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  Roshambo
+//
+//  Created by Jianfeng Yang on 3/1/16.
+//  Copyright © 2016 Jianfeng Yang. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Roshambo/ResultViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultViewController.swift
+//  Roshambo
+//
+//  Created by Jianfeng Yang on 3/1/16.
+//  Copyright © 2016 Jianfeng Yang. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
